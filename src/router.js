1b9409a513ce7e476b1ecbad29836690b660fbeb
groot <zhaicf37@163.com>
2018-12-05 23:06:30 +0800

    add Pos.vue

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Pos from './views/Pos.vue'
 
 Vue.use(Router)
 
@@ -10,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'pos',
+      component: Pos
     },
     {
       path: '/about',
---
+++ src/views/Pos.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="Pos">
+		Hello Pos Demo!
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'Pos'
+	}
+</script>
+
+<style>
+</style>
